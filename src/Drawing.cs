72cfe4eef32c6d9b1e9d88a0eb24d874f1870168
Limows <Limows2013@yandex.ru>
2021-09-08 20:41:41 +0300

    Add ico to project

--- src/Drawing.cs
+++ src/Drawing.cs
@@ -43,5 +43,29 @@ namespace Sharp2048
             path.CloseFigure();
             return path;
         }
+
+        public static void SetColor(int Number)
+        {
+            Color NumberColor;
+
+            switch (Number)
+            {
+                case 0: NumberColor = Color.FromArgb(204, 192, 179); break;
+                case 2: NumberColor = Color.FromArgb(238, 228, 218); break;
+                case 4: NumberColor = Color.FromArgb(237, 224, 200); break;
+                case 8: NumberColor = Color.FromArgb(242, 177, 121); break;
+                case 16: NumberColor = Color.FromArgb(245, 149, 99); break;
+                case 32: NumberColor = Color.FromArgb(246, 124, 95); break;
+                case 64: NumberColor = Color.FromArgb(246, 94, 59); break;
+                case 128: NumberColor = Color.FromArgb(237, 207, 114); break;
+                case 256: NumberColor = Color.FromArgb(237, 204, 97); break;
+                case 512: NumberColor = Color.FromArgb(237, 200, 80); break;
+                case 1024: NumberColor = Color.FromArgb(237, 197, 63); break;
+                case 2048: NumberColor = Color.FromArgb(237, 194, 46); break;
+                default: NumberColor = Color.FromArgb(204, 192, 179); break;
+            }
+
+            Parameters.NumberBrush = new SolidBrush(NumberColor);
+        }
     }
 }
