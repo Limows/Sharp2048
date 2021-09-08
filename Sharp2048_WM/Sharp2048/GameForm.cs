d595347509a6ab33af1cedaea50a982641da5266
Limows <Limows2013@yandex.ru>
2021-09-08 20:25:18 +0300

    Fix build

--- Sharp2048_WM/Sharp2048/GameForm.cs
+++ Sharp2048_WM/Sharp2048/GameForm.cs
@@ -65,30 +65,6 @@ namespace Sharp2048
             return true;
         }
 
-        static void SetColor(int Number)
-        {
-            Color NumberColor;
-
-            switch (Number)
-            {
-                case 0: NumberColor = Color.FromArgb(204, 192, 179); break;
-                case 2: NumberColor = Color.FromArgb(238, 228, 218); break;
-                case 4: NumberColor = Color.FromArgb(237, 224, 200); break;
-                case 8: NumberColor = Color.FromArgb(242, 177, 121); break;
-                case 16: NumberColor = Color.FromArgb(245, 149, 99); break;
-                case 32: NumberColor = Color.FromArgb(246, 124, 95); break;
-                case 64: NumberColor = Color.FromArgb(246, 94, 59); break;
-                case 128: NumberColor = Color.FromArgb(237, 207, 114); break;
-                case 256: NumberColor = Color.FromArgb(237, 204, 97); break;
-                case 512: NumberColor = Color.FromArgb(237, 200, 80); break;
-                case 1024: NumberColor = Color.FromArgb(237, 197, 63); break;
-                case 2048: NumberColor = Color.FromArgb(237, 194, 46); break;
-                default: NumberColor = Color.FromArgb(204, 192, 179); break;
-            }
-
-            Parameters.NumberBrush = new SolidBrush(NumberColor);
-        }
-
         public void AddNumber(int Number)
         {
             Random RandomInit = new Random();
@@ -142,7 +118,7 @@ namespace Sharp2048
 
                     for (int j = 0; j < n; j++)
                     {
-                        SetColor(GameMatrix[i, j]);
+                        Drawing.SetColor(GameMatrix[i, j]);
 
                         Rectangle NumberRect = new Rectangle
                         (
@@ -165,7 +141,7 @@ namespace Sharp2048
             sf.LineAlignment = StringAlignment.Center;
             sf.Alignment = StringAlignment.Center;
 
-            Paint.SmoothingMode = SmoothingMode.AntiAlias;
+            //Paint.SmoothingMode = SmoothingMode.AntiAlias;
 
             if (Parameters.Pallete == Parameters.PalleteType.Rounded)
             {
